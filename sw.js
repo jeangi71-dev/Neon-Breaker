91b1dc7e6f3d5316d851f32901504e7dc6b9fd24
jeangi71-dev <jeangi71@gmail.com>
2026-09-20 01:15:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-const CACHE='neon-breaker-v1';
+const CACHE='neon-breaker-v3';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(fetch(event.request).then(r=>{
+    const copy=r.clone();
+    caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});
+    return r;
+  }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
+});
